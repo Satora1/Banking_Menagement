2974a7134ac79fad4a7e89895592fe0318eae689
Jakub Satora <kubasatora@wp.pl>
2024-05-13 18:29:37 +0200

    labels for log in

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,9 +2,43 @@
 import Link from 'next/link'
 import React, { useState } from 'react'
 import Image from "next/image"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Custominput from './Custominput'
+import { authFormSchema } from '@/lib/utils'
+
 
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null)
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof authFormSchema>>({
+        resolver: zodResolver(authFormSchema),
+        defaultValues: {
+            email: "",
+            password:""
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof authFormSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
     return (
         <section className='auth-form'>
             <header className='flex flex-col gap-5 md:gap-8'>
@@ -33,16 +67,32 @@ const AuthForm = ({ type }: { type: string }) => {
 
                 </div>
             </header>
-            {user?(
+            {user ? (
                 <div className='flex flex-col gap-4'>
-{}
+                    { }
                 </div>
-            ):
-        (
-            <>
-            FORM
-            </>
-        )}
+            ) :
+                (
+                    <>
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <Custominput
+                                control={form.control}
+                                name="email"
+                                label="email"
+                                placeholder="Ener your emial"
+                                />
+                                <Custominput
+                                control={form.control}
+                                name="password"
+                                label="password"
+                                placeholder="Ener your password"
+                                />
+                                <Button type="submit">Submit</Button>
+                            </form>
+                        </Form>
+                    </>
+                )}
         </section>
     )
 }
