c5466e8e3f20aa22956bbddd56d9ea1a7fefebbe
Jakub Satora <kubasatora@wp.pl>
2024-05-14 19:22:15 +0200

    sig in/up form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,25 +18,30 @@ import {
 import { Input } from "@/components/ui/input"
 import Custominput from './Custominput'
 import { authFormSchema } from '@/lib/utils'
+import { Loader2 } from 'lucide-react'
 
 
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null)
-
+    const [isLoading, setIsLoading] = useState(false)
+    
+    const formSchema = authFormSchema(type)
     // 1. Define your form.
-    const form = useForm<z.infer<typeof authFormSchema>>({
-        resolver: zodResolver(authFormSchema),
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
         defaultValues: {
             email: "",
-            password:""
+            password: ""
         },
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof authFormSchema>) {
+    function onSubmit(values: z.infer<typeof formSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
+        setIsLoading(true)
         console.log(values)
+        setIsLoading(false)
     }
 
     return (
@@ -76,21 +81,98 @@ const AuthForm = ({ type }: { type: string }) => {
                     <>
                         <Form {...form}>
                             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                            <Custominput
-                                control={form.control}
-                                name="email"
-                                label="email"
-                                placeholder="Ener your emial"
+                                {type === "sign-up" && (
+                                    <>
+                                        <div className='flex gap-4'>
+                                            <Custominput
+                                                control={form.control}
+                                                name="firstName"
+                                                label="First name"
+                                                placeholder="Ener your First Name"
+                                            />
+                                            <Custominput
+                                                control={form.control}
+                                                name="lastName"
+                                                label="Last name"
+                                                placeholder="Ener your Last Name"
+                                            />
+                                        </div>
+                                        <Custominput
+                                            control={form.control}
+                                            name="address1"
+                                            label="Addres"
+                                            placeholder="Ener your address"
+                                        />
+                                        <div className='flex gap-4'>
+                                            <Custominput
+                                                control={form.control}
+                                                name="state"
+                                                label="State"
+                                                placeholder="Example: Malopolska"
+                                            />
+                                            <Custominput
+                                                control={form.control}
+                                                name="postalCode"
+                                                label="Postal Code"
+                                                placeholder="Example: 33-090"
+                                            />
+                                        </div>
+                                        <div className='flex gap-4'>
+                                            <Custominput
+                                                control={form.control}
+                                                name="dateOfBirth"
+                                                label="Date of Birth"
+                                                placeholder="YYYY-MM-DD"
+                                            />
+                                            <Custominput
+                                                control={form.control}
+                                                name="ssn"
+                                                label="SSN"
+                                                placeholder="Example: 1234"
+                                            />
+                                        </div>
+                                    </>
+                                )}
+                                <Custominput
+                                    control={form.control}
+                                    name="email"
+                                    label="email"
+                                    placeholder="Ener your emial"
                                 />
                                 <Custominput
-                                control={form.control}
-                                name="password"
-                                label="password"
-                                placeholder="Ener your password"
+                                    control={form.control}
+                                    name="password"
+                                    label="password"
+                                    placeholder="Ener your password"
                                 />
-                                <Button type="submit">Submit</Button>
+                                <div className='flex flex-col gap-4'>
+                                    <Button type="submit" disabled={isLoading}
+                                        className='form-btn'>{
+                                            isLoading ? (
+                                                <>
+                                                    <Loader2 size={20}
+                                                        className="animate-spin"
+                                                         />
+                                                    &nbsp;
+                                                    Loading...
+                                                </>
+                                            ) :
+                                                type === "sign-in" ?
+                                                    "Sign In" : "Sign Up"}</Button>
+                                </div>
+
                             </form>
                         </Form>
+                        <footer className='flex justify-center gap-1'>
+                            <p className='text-14 font-normal text-gray-600'>
+                                {type === "sig-in" ?
+                                    "Don't have an account?" :
+                                    "Already have an account?"}
+                            </p>
+                            <Link href={type === "sign-in" ? "/sign-up" : "/sign-in"} className='form-link'>
+                                {type === "sign-in" ? "Sign Up" : "Sign In"}
+                            </Link>
+                        </footer>
                     </>
                 )}
         </section>
