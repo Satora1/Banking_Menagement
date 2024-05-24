f58b783642bdbf0f8357f8680ef8663ee3ea5a4b
Jakub Satora <kubasatora@wp.pl>
2024-05-24 15:24:00 +0200

    add bank info

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -6,11 +6,15 @@ import { Doughnut } from 'react-chartjs-2'
 ChartJS.register(ArcElement, Tooltip, Legend)
 
 const DoughnutChart = ({accounts}:DoughnutChartProps) => {
+
+    const accountNames=accounts.map((a)=>a.name)
+    const balances= accounts.map((a)=>a.currentBalance)
+
     const data={
         datasets:[
             {
                 label:"Banks",
-                data:[1243,3443,5223],
+                data:balances,
                 backgroundColor:["#0747b6","#2265d8","#2265d8","#2f91fa"]
             }
         ],
